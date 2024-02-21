7c3a76a14435ce29d7e7ae003bb158f393b212e9
Pelayo <phuermij@myuax.com>
2024-02-21 17:46:41 +0100

    cambios

--- Poisson2.py
+++ Poisson2.py
@@ -22,17 +22,16 @@ for i in range(N):
 # Aquí introducimos los datos de contorno
 for i in range(1, N):
     w[i][0] = 0
-    w[i][M] = 0
+    w[i][M] = i**2
 
 for j in range(1, M):
-    w[0][j] = 0
+    w[0][j] = 1-j**2
     w[N][j] = 1
 
 for k in range(100):
     for i in range(1, N):
         for j in range(1, M):
-            w[i][j] = (k ** 2 * (w[i + 1][j] + w[i - 1][j]) + h ** 2 * (w[i][j + 1] + w[i][j - 1]) - (
-                        h * k) ** 2 * f(i, j)) / (2 * (h ** 2 + k ** 2))
+            w[i][j] = (k ** 2 * (w[i + 1][j] + w[i - 1][j]) + h ** 2 * (w[i][j + 1] + w[i][j - 1]) - (h * k) ** 2 * f(i, j)) / (2 * (h ** 2 + k ** 2))
 
 # Definir los puntos x, y, z para la superficie
 x = np.linspace(a, b, M + 1)
